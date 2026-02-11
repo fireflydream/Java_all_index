30329472da0fed7dddd66178467a9f9b335c0b9f
doraemons-dream <584063844@qq.com>
2026-02-11 23:17:10 +0800

    2026年2月11日

---
+++ hanshunping/day01_comment/src/com/weirui/debug/Debug02.java
@@ -0,0 +1,11 @@
+package com.weirui.debug;
+
+public class Debug02 {
+    public static void main(String[] args) {
+        // 初始化一个一维数组arr,数据类型为int
+        int[] arr = {1, 3, 10, -1};
+        for (int i = 0; i < arr.length; i++) {
+            System.out.println("arr[" + i + "] = " + arr[i]);
+        }
+    }
+}
